7974ad3be9330fe22e0c466c84a0a471ec133975
Myles Slack <binglaplap@gmail.com>
2026-02-03 17:15:31 -0500

    Initial Commit

---
+++ NotInstagram/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NotInstagram
+//
+//  Created by Myles Slack on 2026.02.03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NotInstagram/NotInstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotInstagramApp.swift
+//  NotInstagram
+//
+//  Created by Myles Slack on 2026.02.03.
+//
+
+import SwiftUI
+
+@main
+struct NotInstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
