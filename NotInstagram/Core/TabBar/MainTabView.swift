9775a7d53f97941886f14452d61394da70791b15
Myles Slack <binglaplap@gmail.com>
2026-02-03 18:14:13 -0500

    Set up the MainTabView and buttons

---
+++ NotInstagram/Core/TabBar/MainTabView.swift
@@ -0,0 +1,40 @@
+//
+//  MainTabView.swift
+//  NotInstagram
+//
+//  Created by Myles Slack on 2026.02.03.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView{
+            Text("Feed")
+                .tabItem {
+                    Image(systemName: "house")
+                }
+            Text("Search")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                }
+            Text("Uplaoad Post")
+                .tabItem {
+                    Image(systemName: "plus.square")
+                }
+            Text("Notifications")
+                .tabItem {
+                    Image(systemName: "heart")
+                }
+            Text("Profile")
+                .tabItem {
+                    Image(systemName: "person")
+                }
+        }
+        .accentColor(.black)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
