bef1f6d65079d89928844102ef1d88020402a5e3
Myles Slack <binglaplap@gmail.com>
2026-02-03 21:33:50 -0500

    Added a scrollview that takes a lvStack that gets a list of FeedCells into the FeedView. added a navigation stack and placed all of the FeedView conent inside of it. then added a NotInstagram logo in the navigation toolbar item. finally added the FeedView inside of the mainTabView

--- NotInstagram/Core/Feed/FeedView.swift
+++ NotInstagram/Core/Feed/FeedView.swift
@@ -9,7 +9,25 @@ import SwiftUI
 
 struct FeedView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView{
+                LazyVStack(spacing: 32){
+                    ForEach(0 ... 10, id: \.self){ post in
+                        FeedCell()
+                    }
+                }
+                .padding(.top, 8)
+            }
+            .navigationTitle("Feed")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar{
+                ToolbarItem(placement: .navigationBarLeading){
+                    Image("NotIGWorse")
+                        .resizable()
+                        .frame(width: 100, height: 40)
+                }
+            }
+        }
     }
 }
 
