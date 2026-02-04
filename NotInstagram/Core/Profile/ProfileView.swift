dea7da74e6f165dab3954679b6e8b05081922572
Myles Slack <binglaplap@gmail.com>
2026-02-03 20:12:11 -0500

    In ProfileView I put the outer ScrollView inside of a Navigation stack with a profile navigation title and a hamburger menu button. I also on the MainTabVie added the ProfileView to the profile tab button.

--- NotInstagram/Core/Profile/ProfileView.swift
+++ NotInstagram/Core/Profile/ProfileView.swift
@@ -16,69 +16,83 @@ struct ProfileView: View {
     ]
     
     var body: some View {
-        ScrollView {
-            //Header
-            VStack(spacing: 10) {
+        NavigationStack {
+            ScrollView {
+                //Header
+                VStack(spacing: 10) {
+                    
+                    //pictures and stats
+                    HStack {
+                        Image("meHongKongBeach")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 80, height: 80)
+                            .clipShape(Circle())
+                            .shadow(radius: 10)
+                        
+                        Spacer()
+                        
+                        HStack(spacing: 8){
+                            
+                            UserStatView(value: 123, title: "Posts")
+                            
+                            UserStatView(value: 456, title: "Followers")
+                            
+                            UserStatView(value: 789, title: "Following")
+                        }
+                    }
+                    .padding(.vertical)
+                    .padding(.horizontal)
+                    
+                    //Name and Bio
+                    VStack(alignment: .leading , spacing: 4) {
+                        Text("Myles Slack")
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                        
+                        Text("Im sliving my life like its golden!")
+                            .font(.footnote)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal)
+                    
+                    // Action button
+                    Button{
+                        //Action
+                    }label: {
+                        Text("Edit Profile")
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                            .frame(width: 360, height: 32)
+                            .foregroundColor(.black)
+                            .overlay(RoundedRectangle(cornerRadius: 6)
+                                .stroke(Color.gray, lineWidth: 1)
+                            )
+                    }
+                    
+                    Divider()
+                }
                 
-                //pictures and stats
-                HStack {
-                    Image("meHongKongBeach")
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 80, height: 80)
-                        .clipShape(Circle())
-                        .shadow(radius: 10)
-                    
-                    Spacer()
-                    
-                    HStack(spacing: 8){
-                        
-                        UserStatView(value: 123, title: "Posts")
-                        
-                        UserStatView(value: 456, title: "Followers")
-                        
-                        UserStatView(value: 789, title: "Following")
+                // Post grid View
+                
+                LazyVGrid(columns: grideItems, spacing: 1) {
+                    ForEach(0 ... 15, id: \.self) { index in
+                        Image("FavoriteMoonCake")
+                            .resizable( )
+                            .scaledToFill( )
                     }
                 }
-                .padding(.vertical)
-                .padding(.horizontal)
-                
-                //Name and Bio
-                VStack(alignment: .leading , spacing: 4) {
-                    Text("Myles Slack")
-                        .font(.footnote)
-                        .fontWeight(.semibold)
-                    
-                    Text("Im sliving my life like its golden!")
-                        .font(.footnote)
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.horizontal)
-                
-                // Action button
-                Button{
-                    //Action
-                }label: {
-                    Text("Edit Profile")
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-                        .frame(width: 360, height: 32)
-                        .foregroundColor(.black)
-                        .overlay(RoundedRectangle(cornerRadius: 6)
-                            .stroke(Color.gray, lineWidth: 1)
-                        )
-                }
-                
-                Divider()
             }
-            
-            // Post grid View
-            
-            LazyVGrid(columns: grideItems, spacing: 1) {
-                ForEach(0 ... 15, id: \.self) { index in
-                    Image("FavoriteMoonCake")
-                        .resizable( )
-                        .scaledToFill( )
+            .navigationTitle("Profile")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing){
+                    Button{
+                        
+                    } label: {
+                        Image(systemName: "line.3.horizontal")
+                            .foregroundColor(.black)
+                    }
                 }
             }
         }
