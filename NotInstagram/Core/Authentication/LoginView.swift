788617ef6a482210ce7d9386d7dd29f559f7533a
Myles Slack <binglaplap@gmail.com>
2026-02-17 20:18:18 -0500

    Added a LoginView view inside of the authentication sections in file architecture that includes a NotInstagram logo, an email TextFeild, and a SecureField all inside of a NavigationStack

---
+++ NotInstagram/Core/Authentication/LoginView.swift
@@ -0,0 +1,45 @@
+//
+//  LoginView.swift
+//  NotInstagram
+//
+//  Created by Myles Slack on 2026.02.15.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Image("NotIGWorse")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 220, height: 100)
+                
+                VStack {
+                    TextField("Email", text: $email)
+                        .autocapitalization(.none)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 24)
+                    
+                    SecureField("Password", text: $password)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 24)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
